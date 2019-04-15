e3f38803b0c8a5639f2fbf727661ff1d8c714f23
shisk <511411764@mapb.com>
2019-04-15 19:33:42 +0800

    gl

--- app/src/main/java/pinetree/lifenavi/shader/TextureStyle.java
+++ app/src/main/java/pinetree/lifenavi/shader/TextureStyle.java
@@ -6,6 +6,7 @@ import android.opengl.GLSurfaceView;
 import java.nio.FloatBuffer;
 
 import pinetree.lifenavi.utils.ShaderUtil;
+import pinetree.lifenavi.utils.VBOHelper;
 
 public class TextureStyle {
 
@@ -18,6 +19,15 @@ public class TextureStyle {
     private FloatBuffer vBuffer;
     private FloatBuffer cBuffer;
 
+    private float tRange;
+    private float sRange;
+
+    public TextureStyle(float tRange, float sRange, GLSurfaceView glSurfaceView) {
+        this.tRange = tRange;
+        this.sRange = sRange;
+        init(glSurfaceView);
+    }
+
     private void init(GLSurfaceView glSurfaceView) {
         String vertex = ShaderUtil.loadFromAssetsFile("tt_v.sh", glSurfaceView.getResources());
         String frag = ShaderUtil.loadFromAssetsFile("tt_frag.sh", glSurfaceView.getResources());
@@ -26,9 +36,18 @@ public class TextureStyle {
         vertexHandler = GLES30.glGetAttribLocation(progeram, "vPosition");
         colorHandler = GLES30.glGetAttribLocation(progeram, "aTextCoordiate");
 
-        float[] vs=new float[]{
+        final float U_SIZE = 0.3f;
 
+        float[] vs = new float[]{
+                -4 * U_SIZE, 4 * U_SIZE, 0,
+                -4 * U_SIZE, -4 * U_SIZE, 0,
+                4 * U_SIZE, -4 * U_SIZE, 0,
+                4 * U_SIZE, 4 * U_SIZE, 0
         };
+
+        vBuffer = VBOHelper.getFloagBufferData(vs);
+
+        float textCoord[] = new float[]{0, 0,};
     }
 
 }
